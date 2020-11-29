520ea9099b736990c5c3af57ffc710a40dfe9d76
Eduardo Tongson <propolice@gmail.com>
2020-11-29 19:47:44 +0800

    src: Environment did not work well.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::env;
 
 #[tokio::main]
 async fn main() {
-  let d = env::var("DIR").unwrap_or(".".to_string());
-  let p: u16 = env::var("PORT").unwrap_or_else(|_| "3030".to_string()).parse::<u16>().unwrap();
+  let d = env::args().nth(1).unwrap_or(".".to_string());
+  let p: u16 = env::args().nth(2).unwrap_or("3030".to_string()).parse::<u16>().unwrap();
   warp::serve(warp::fs::dir(d))
     .run(([127, 0, 0, 1], p))
     .await;
