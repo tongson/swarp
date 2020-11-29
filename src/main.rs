cb1c621d3b3836ee4f8fba8c6e29185d8fafc111
Eduardo Tongson <propolice@gmail.com>
2020-11-29 19:37:06 +0800

    src: Get directory and port from environment.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #![deny(warnings)]
+use std::env;
 
 #[tokio::main]
 async fn main() {
-    pretty_env_logger::init();
-
-    warp::serve(warp::fs::dir("."))
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+  let d = env::var("DIR").unwrap_or(".".to_string());
+  let p: u16 = env::var("PORT").unwrap_or_else(|_| "3030".to_string()).parse::<u16>().unwrap();
+  warp::serve(warp::fs::dir(d))
+    .run(([127, 0, 0, 1], p))
+    .await;
 }
