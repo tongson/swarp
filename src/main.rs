b59da7f26437513d9111ab46b23a6c802c6b4645
Eduardo Tongson <propolice@gmail.com>
2020-12-12 18:59:19 +0800

    Correct port configuration.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use std::net::IpAddr;
 #[tokio::main]
 async fn main() {
   let bind = env::var("BIND").unwrap_or("127.0.0.1".to_string());
-  let port = env::var("PORT").unwrap_or("3030".to_string()).parse::<u16>().unwrap();
+  let port = env::var("PORT").unwrap_or("3030".to_string());
   let cwd = env::var("CWD").unwrap_or(".".to_string());
   warp::serve(warp::fs::dir(cwd))
-    .run((bind.parse::<IpAddr>().unwrap(), port))
+    .run((bind.parse::<IpAddr>().unwrap(), port.parse::<u16>().unwrap()))
     .await;
 }
