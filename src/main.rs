b438e3041e55a71e68be82b2218739e2c9868537
Eduardo Tongson <propolice@gmail.com>
2020-12-12 19:27:03 +0800

    Renamed to swarp.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::net::IpAddr;
 
 #[tokio::main]
 async fn main() {
-  let bind = env::var("WARP_BIND").unwrap_or("127.0.0.1".to_string());
-  let port = env::var("WARP_PORT").unwrap_or("3030".to_string());
-  let cwd = env::var("WARP_CWD").unwrap_or(".".to_string());
+  let bind = env::var("SWARP_BIND").unwrap_or("127.0.0.1".to_string());
+  let port = env::var("SWARP_PORT").unwrap_or("3030".to_string());
+  let cwd = env::var("SWARP_CWD").unwrap_or(".".to_string());
   warp::serve(warp::fs::dir(cwd))
     .run((bind.parse::<IpAddr>().unwrap(), port.parse::<u16>().unwrap()))
     .await;
